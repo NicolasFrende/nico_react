b004a24ef9f75028fd7368b1e7540703deb2a032
NicolasFrende <nicofrende@gmail.com>
2025-02-05 20:53:47 -0300

    safe funciona

--- nico_react/src/App.jsx
+++ nico_react/src/App.jsx
@@ -1,36 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "bootstrap-icons/font/bootstrap-icons.css"
-import { Navbar_Componente } from './Componentes/navbar/Navbar_Componente'
-import { Footer_Componente } from './Componentes/footer/Footer_Componente'
-import { Products_Componente } from './Componentes/products/Products_Componente'
-import { About_Componente } from './Componentes/about/About_componente'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Navbar_Componente } from "./Componentes/01 - navbar/Navbar_Componente";
+import { Footer_Componente } from "./Componentes/04- footer/Footer_Componente";
+import { Products_Componente } from "./Componentes/02  - products/Products_Componente";
+import { About_Componente } from "./Componentes/03 - about/About_componente";
 
 function App() {
-  
-
   return (
-    <> 
-    <section>
-      <Navbar_Componente/>
-      <Products_Componente/>
-      <About_Componente/>
-      <Footer_Componente/>  
-    </section>    
-      
-      
+    <>
+      <section>
+        <Navbar_Componente />
+        <Products_Componente />
+        <About_Componente />
+        <Footer_Componente />
+      </section>
     </>
-  )
+  );
 }
 
-export default App   
-      
-      
-      
-      
-     /* <div>
+export default App;
+
+/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ nico_react/src/Componentes/02  - products/Products_Componente.jsx
@@ -0,0 +1,283 @@
+import Button from "@material-ui/core/Button";
+import ATR_MP_Logo_Color_Bccc from "../../assets/images/ATR_MP_Logo_Color_Bccc.png";
+import "./Products_Componente.css";
+export const Products_Componente = () => {
+  return (
+    <>
+       <div id="products" className="galeria_productos"> 
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025
+          </div>
+          <div className="producto_precio">150 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 1
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 2
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 3
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 4
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 5
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 6
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 7
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 8
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 9
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 10
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 11
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+
+        <div className="Producto">
+          <div className="producto_imagen">
+            <img className="logo_prod" src={ATR_MP_Logo_Color_Bccc}></img>
+          </div>
+          <div className="producto_texto">
+            All Nico Frende Road Sets - Season 1 2025 week 12
+          </div>
+          <div className="producto_precio">15 $</div>
+          <div className="boton_ADDcarrito">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                alert("test");
+              }}
+            >
+              add to cart
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
